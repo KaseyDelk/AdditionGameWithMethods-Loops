b64f6ea6bb2bb1b442d693d4097aefdc0081ab2f
KaseyDelk <delkk@student.swosu.edu>
2015-11-11 09:52:48 -0600

    finished writing code. still needs cleaning

--- src/AdditionGameWithMethodsLoops.java
+++ src/AdditionGameWithMethodsLoops.java
@@ -1,51 +1,62 @@
 import java.util.Scanner;
+
 public class AdditionGameWithMethodsLoops {
 	public static void main(String[] args) {
 		
-		// call the addition game method
-		additionGameMethod();	
-		
-		System.out.println("All done.");
+		// call addition game method
+		AdditonGameMethod();
 	}
-	public static void additionGameMethod() {
-		System.out.println("Inside the addition game method.");
+	public static void AdditonGameMethod() {
 		
-		// set up the for loop
-		int numberOfRounds = 2;
-		int hardness  = 5;
-
-		for (int roundNumber = 1;
-		roundNumber <= numberOfRounds;
+		int hardness = 5;
+		int hardnessStep = 2;
+		int score = 0;
+		
+		// create for loop for running rounds
+		int numberOfRounds = 4;
+		for(int roundNumber = 1; 
+		roundNumber <= numberOfRounds;  
 		roundNumber = roundNumber + 1){
-			System.out.println("Inside the for loop. Round : " + roundNumber);
+			System.out.println("Round " + roundNumber);
 			boolean isAnswerCorrect = getAndCheckStudentAnswer(hardness);
-			
-		System.out.println("Hello!");
-		isAnswerCorrect = getAndCheckStudentAnswer(hardness);
-		if (isAnswerCorrect){
-			System.out.println("It was correct.");
-		}else{
-			System.out.println("It was not incorrect.");
-		}
-		
-	}
-		return;
-		
-	}
-	public static boolean getAndCheckStudentAnswer(int hardness) {
-		System.out.println("Inside get and check student answer method.");
-		int number1 = (int)(Math.random()*hardness);
-		int number2 = (int)(Math.random()*hardness);
-		System.out.println("Add " + number1 + " + " + number2 + ":");
-		Scanner input = new Scanner(System.in);
-		int studentAnswer = input.nextInt();
-		if (studentAnswer == (number1 + number2)){
-			System.out.println("Good job! Your answer was correct.");
-			return true;
-		}else{
-			System.out.println("Nice try, but the correct answer " +
-					"was " + (number1 + number2));
-			return false;
+			if(isAnswerCorrect){
+				System.out.print("Your score was " + score + " and is now ");
+				score = score + hardness;
+				System.out.println(score + ".");
+				
+				if(roundNumber<numberOfRounds){
+					System.out.print("Your hardness was " + hardness + " and is now ");
+					hardness = hardness * hardnessStep;
+					System.out.println(hardness + ".");
+				}
+			}else{
+				if(roundNumber<numberOfRounds){
+					System.out.print("Your hardness was " + hardness + " and is now ");
+					if(hardness>5){
+						hardness = hardness / hardnessStep;
+					}
+					System.out.println(hardness + ".");
+				}
 			}
 		}
+		System.out.println("The game is complete.");
+		System.out.println("Your final score was " + score );
 	}
+	
+	public static boolean getAndCheckStudentAnswer(int hardness) {
+		int number1 = (int)(Math.random()*hardness);
+		int number2 = (int)(Math.random()*hardness);
+		System.out.println("Add " + number1 + " + " + number2);
+		System.out.println("Please enter integars only.");
+	
+		Scanner get = new Scanner(System.in);
+		int studentAnswer = get.nextInt();
+		if(studentAnswer == (number1 + number2)){
+			System.out.println("Good work, your answer was correct.");
+			return true;
+		}else{
+			System.out.println("Nice try, but the correct answer was " + (number1 + number2));
+			return false;
+		}
+	}
+}
